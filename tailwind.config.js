c321a1cb3b1f7c7592ab3047367a3d84e196b210
Cesar Alberto Huerta Aguilar <cesar.a.huerta.a@gmail.com>
2026-05-13 19:34:43 -0600

    Fix Tailwind v4 - CSS classes ahora aplican correctamente

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        brand: {
-          dark: '#04080f',
-          accent: '#2e5cb8',
-          accentLight: '#3d6fd0'
-        }
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
