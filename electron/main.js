8930af592fd16172abccfc1011ab1346ff1f99cc
Cesar Alberto Huerta Aguilar <cesar.a.huerta.a@gmail.com>
2026-09-05 15:31:06 -0600

    fix: CORS for Electron - intercept Supabase requests to replace null origin

--- electron/main.js
+++ electron/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, shell } = require('electron');
+const { app, BrowserWindow, shell, session } = require('electron');
 const path = require('path');
 const url  = require('url');
 
@@ -41,6 +41,27 @@ function createWindow() {
 }
 
 app.whenReady().then(function() {
+  // FIX CORS: cuando la app corre desde file://, el Origin es null y Supabase lo rechaza.
+  // Interceptamos las peticiones salientes a Supabase y ponemos un Origin válido.
+  session.defaultSession.webRequest.onBeforeSendHeaders(
+    { urls: ['https://mciyywpqihnxhvqbznmq.supabase.co/*'] },
+    function(details, callback) {
+      details.requestHeaders['Origin'] = 'https://mciyywpqihnxhvqbznmq.supabase.co';
+      callback({ requestHeaders: details.requestHeaders });
+    }
+  );
+
+  // Permitir las respuestas CORS de Supabase aunque el Origin sea file://
+  session.defaultSession.webRequest.onHeadersReceived(
+    { urls: ['https://mciyywpqihnxhvqbznmq.supabase.co/*'] },
+    function(details, callback) {
+      const headers = Object.assign({}, details.responseHeaders);
+      headers['Access-Control-Allow-Origin'] = ['*'];
+      headers['Access-Control-Allow-Headers'] = ['*'];
+      callback({ responseHeaders: headers });
+    }
+  );
+
   createWindow();
   app.on('activate', function() {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
